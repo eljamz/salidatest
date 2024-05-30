043f4d04f69eb31b3b6db99f82945469347b6110
Agustin Marmolejo <eljamz@gmail.com>
2024-05-29 19:34:43 -0600

    fix: cleanup

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,23 +4,11 @@ export default class extends Controller {
   static targets = [ "rsvpId" ]
 
   connect() {
-    console.log('connect method called')
     this.modal = new bootstrap.Modal(this.element)
-    console.log('Modal instance created:', this.modal)
-  
-    this.element.addEventListener('show.bs.modal', () => {
-      console.log('Modal is being shown')
-    })
-  
-    this.element.addEventListener('hide.bs.modal', () => {
-      console.log('Modal is being hidden')
-    })
   
     const newRsvpId = this.element.dataset.newRsvpId
     if (newRsvpId) {
-      console.log('Showing modal...')
       this.modal.show()
-      console.log('New RSVP ID:', newRsvpId)
       this.rsvpIdTarget.textContent = `New RSVP ID: ${newRsvpId}`
     }
   }
